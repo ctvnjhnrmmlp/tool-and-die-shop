e928633b0d7f295d700ff63c7e92cd1e5838c7cb
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-03-13 11:39:18 +0800

    fix: updated product images

--- database/tool-and-die/models/Product.model.ts
+++ database/tool-and-die/models/Product.model.ts
@@ -1,6 +1,12 @@
 import { toolAndDieDatabase } from '../tool-and-die.database';
 
 const ProductModel = {
+	addProduct: async (product: Product) => {
+		await toolAndDieDatabase.products.add(product);
+	},
+	updateProduct: async (product: Product) => {
+		await toolAndDieDatabase.products.update(product.id, product);
+	},
 	deleteProduct: async (product: Product) => {
 		await toolAndDieDatabase.products.delete(product.id);
 	},
